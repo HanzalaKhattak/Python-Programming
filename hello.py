d0a22089664d47ce61894eb3b08d88251e399a0f
Hanzala Khattak <muhammadhanzalahkhan1@gmail.com>
2025-08-30 16:26:37 +0500

    Today marks the start of the journy of learning The Syntax of Python

---
+++ hello.py
@@ -0,0 +1,10 @@
+# code hello.py => Command to create a new repo called hello.py
+# python hello.py => Command to Run a File
+print("Heyyy! Wassup?");
+print("What's your name?");
+print("My Name's Deez");
+print("Deez who?");
+print("Whatever Man. Have a nice Day");
+
+
+input()
